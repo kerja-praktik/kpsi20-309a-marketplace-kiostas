b2d2ed65fc9d740994007a2d914176edc8b3b892
jessycha tampubolon <jessycha.royanti@gmail.com>
2020-07-24 08:32:01 +0700

    menambahkan halaman index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Index</title>
+<style>
+.template{
+  height:380px;
+  padding-left:40px;
+}
+</style>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+ <!-- Navigation bar -->
+ @extends('navbar')
+<body>
+<!-- Content  -->
+<div class="container-fluid" style="margin-top:120px">
+<div class = "template">
+<form>
+</form>
+</div>
+  </div>
+</body>
+<div class=" " style="margin-top:150px">
+   <!-- footer -->
+   @extends('footer')
+</html>
