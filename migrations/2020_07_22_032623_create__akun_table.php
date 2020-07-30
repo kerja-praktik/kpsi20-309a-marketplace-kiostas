29fc45dd7caa6355cfd07afa115388272d946803
Tripheni-hepriana <55095443+Tripheni-hepriana@users.noreply.github.com>
2020-07-30 14:04:24 +0700

    Add files via upload

---
+++ migrations/2020_07_22_032623_create__akun_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAkunTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_akun', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->string('no_hp');
+            $table->string('kata_sandi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_akun');
+    }
+}
---
+++ migrations/2020_07_22_032724_create_toko_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('toko', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('telepon');
+            $table->string('email')->unique();
+            $table->string('kategori_produk');
+            $table->time('tahun_berdiri');
+            $table->string('motto/slogan');
+            $table->string('alamat');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('toko');
+    }
+}
