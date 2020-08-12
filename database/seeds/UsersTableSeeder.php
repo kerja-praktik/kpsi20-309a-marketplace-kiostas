1eaec268c40db8a5990c431acb9439b18c61f1f4
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-12 08:17:42 +0700

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,29 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
 
-class UsersTableSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        User::create([
-            'name'      => 'Maria Manullang',
-<<<<<<< HEAD
-            'no_hp'     => '087715150148',
-            'email'     => 'mariamanullang@gmail.com',
-            'password'  =>  Hash::make('baru1308'),
-=======
-            'no_hp'     => '0878657899',
-            'email'     => 'mariamanullag@gmail.com',
-            'password'  =>  Hash::make('calalala'),
->>>>>>> 3243b13aedbdde5529552884aeba48f7b2fd2901
-            
-        ]);
+        // $this->call(UsersTableSeeder::class);
     }
 }
