090ee2ab1b5143f4057e173caeff27ce86d36cbd
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-18 15:08:04 +0700

    Update OrdersController.php

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Cart_model;
 use App\Orders_model;
+use App\ListOrders_model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
