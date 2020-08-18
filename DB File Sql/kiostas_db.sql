2d9583062e02cf5498ecee6d89ae44dce286e679
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-18 14:58:33 +0700

    Update kiostas_db.sql

--- DB File Sql/kiostas_db.sql
+++ DB File Sql/kiostas_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2020 at 08:39 AM
+-- Generation Time: Aug 18, 2020 at 09:55 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
