0fc2f7130dad550321ed382ef9ea4bb7ea35b0ff
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-24 11:09:23 +0700

    Update header.blade.php

--- resources/views/backEnd/layouts/header.blade.php
+++ resources/views/backEnd/layouts/header.blade.php
@@ -1,17 +1,17 @@
 <!--Header-part-->
-<div id="header">
-    <h1><a href="dashboard.html">Laravel Admin</a></h1>
+<div id="header" style="margin-top:30px">
+<a href="{{url('/admin')}}"> <img src="{{asset('frontEnd/images/home/Kiosta.png')}}" alt="Image" height="40" width="200"></a>
 </div>
 <!--close-Header-part-->
 <!--top-Header-menu-->
 <div id="user-nav" class="navbar navbar-inverse">
     <ul class="nav">
-        <li class=""><a title="" href="{{url('/admin/settings')}}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
+        <!-- <li class=""><a title="" href="{{url('/admin/settings')}}"><i class="icon icon-cog"></i> <span class="text">Pengaturan</span></a></li> -->
         <li class="">
             <a class="dropdown-item" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                <i class="icon icon-share-alt"></i>{{ __('Logout') }}
+                <i class="icon icon-share-alt"></i>{{ __('Keluar') }}
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -24,7 +24,7 @@
 <!--close-top-Header-menu-->
 <!--start-top-serch-->
 <div id="search">
-    <input type="text" placeholder="Search here..."/>
+    <input type="text" placeholder="Temukan..."/>
     <button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white"></i></button>
 </div>
 <!--close-top-serch-->
