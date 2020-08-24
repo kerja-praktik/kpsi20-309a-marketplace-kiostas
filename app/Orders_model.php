25ce73e1fb93b75c710d790cf00fb9587f10fd10
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-24 11:04:05 +0700

    Update Orders_model.php

--- app/Orders_model.php
+++ app/Orders_model.php
@@ -9,5 +9,6 @@ class Orders_model extends Model
     protected $table='orders';
     protected $primaryKey='id';
     protected $fillable=['users_id',
-        'users_email','name','address','city','state','postal_code','country','mobile','shipping_charges','order_status','payment_method','grand_total'];
+        'users_email','name','address','city','province','postal_code','country','mobile','shipping_charges','order_status','payment_method','grand_total','delivery_cost','nomor_hp','order_status', 
+        'products_id'];
 }
