edd705d52a9ccecaaed3049715e4fe5fc93a0f77
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-24 11:03:38 +0700

    Update ListOrders_model.php

--- app/ListOrders_model.php
+++ app/ListOrders_model.php
@@ -8,8 +8,6 @@ class ListOrders_model extends Model
 {
     protected $table='orders';
     protected $primaryKey='id';
-    protected $fillable=['users_id','users_email','phone_number', 'province', 'order_status','payment_method','grand_total','created_at'];
+    protected $fillable=['users_id','users_email','mobile', 'province', 'order_status','payment_method','grand_total','created_at'];
 
 }
-
-
