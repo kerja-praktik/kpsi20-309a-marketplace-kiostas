e25162b230e33b3b9724617dbe33ce974615a5ed
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-24 11:11:30 +0700

    Update edit.blade.php

--- resources/views/backEnd/products/edit.blade.php
+++ resources/views/backEnd/products/edit.blade.php
@@ -10,14 +10,14 @@
         @endif
         <div class="widget-box">
             <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
-                <h5>Add New Products</h5>
+                <h5>Tambah Produk Baru</h5>
             </div>
             <div class="widget-content nopadding">
                 <form action="{{route('product.update',$edit_product->id)}}" method="post" class="form-horizontal" enctype="multipart/form-data">
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
                     {{method_field("PUT")}}
                     <div class="control-group">
-                        <label class="control-label">Select Category</label>
+                        <label class="control-label">Pilih Kategori</label>
                         <div class="controls">
                             <select name="categories_id" style="width: 415px;">
                                 @foreach($categories as $key=>$value)
@@ -37,44 +37,44 @@
                         </div>
                     </div>
                     <div class="control-group">
-                        <label for="p_name" class="control-label">Name</label>
+                        <label for="p_name" class="control-label">Nama</label>
                         <div class="controls{{$errors->has('p_name')?' has-error':''}}">
                             <input type="text" name="p_name" id="p_name" class="form-control" value="{{$edit_product->p_name}}" title="" required="required" style="width: 400px;">
                             <span class="text-danger">{{$errors->first('p_name')}}</span>
                         </div>
                     </div>
                     <div class="control-group">
-                        <label for="p_code" class="control-label">Code</label>
+                        <label for="p_code" class="control-label">Kode Produk</label>
                         <div class="controls{{$errors->has('p_code')?' has-error':''}}">
                             <input type="text" name="p_code" id="p_code" class="form-control" value="{{$edit_product->p_code}}" title="" required="required" style="width: 400px;">
                             <span class="text-danger">{{$errors->first('p_code')}}</span>
                         </div>
                     </div>
                     <div class="control-group">
-                        <label for="p_color" class="control-label">Color</label>
+                        <label for="p_color" class="control-label">Varian</label>
                         <div class="controls{{$errors->has('p_color')?' has-error':''}}">
                             <input type="text" name="p_color" id="p_color" value="{{$edit_product->p_color}}" required="required" style="width: 400px;">
                             <span class="text-danger">{{$errors->first('p_color')}}</span>
                         </div>
                     </div>
                     <div class="control-group">
-                        <label for="description" class="control-label">Description</label>
+                        <label for="description" class="control-label">Deskripsi</label>
                         <div class="controls{{$errors->has('description')?' has-error':''}}">
                             <textarea class="textarea_editor span12" name="description" id="description" rows="6" placeholder="Product Description" style="width: 580px;">{{$edit_product->description}}</textarea>
                             <span class="text-danger">{{$errors->first('description')}}</span>
                         </div>
                     </div>
                     <div class="control-group">
-                        <label for="price" class="control-label">Price</label>
+                        <label for="price" class="control-label">Harga</label>
                         <div class="controls{{$errors->has('price')?' has-error':''}}">
-                            <div class="input-prepend"> <span class="add-on">$</span>
+                            <div class="input-prepend"> <span class="add-on">Rp</span>
                                 <input type="number" name="price" id="price" class="" value="{{$edit_product->price}}" title="" required="required">
                                 <span class="text-danger">{{$errors->first('price')}}</span>
                             </div>
                         </div>
                     </div>
                     <div class="control-group">
-                        <label class="control-label">Image upload</label>
+                        <label class="control-label">Gambar</label>
                         <div class="controls">
                             <input type="file" name="image" id="image"/>
                             <span class="text-danger">{{$errors->first('image')}}</span>
@@ -88,7 +88,7 @@
                     <div class="control-group">
                         <label for="" class="control-label"></label>
                         <div class="controls">
-                            <button type="submit" class="btn btn-success">Edit Product</button>
+                            <button type="submit" class="btn btn-success">Edit</button>
                         </div>
                     </div>
                 </form>
