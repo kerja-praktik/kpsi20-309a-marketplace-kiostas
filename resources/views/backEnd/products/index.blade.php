40234ed27a85fe6dc15d36995d05d72a4a59ddeb
Maria <52289690+Maria-Manullang@users.noreply.github.com>
2020-08-24 11:15:03 +0700

    Update index.blade.php

--- resources/views/backEnd/products/index.blade.php
+++ resources/views/backEnd/products/index.blade.php
@@ -1,31 +1,31 @@
 @extends('backEnd.layouts.master')
-@section('title','List Products')
+@section('title','Daftar Produk')
 @section('content')
-    <div id="breadcrumb"> <a href="{{url('/admin')}}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="{{route('product.index')}}" class="current">Products</a></div>
+    <div id="breadcrumb"> <a href="{{url('/admin')}}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Beranda</a> <a href="{{route('product.index')}}" class="current">Products</a></div>
     <div class="container-fluid">
         @if(Session::has('message'))
             <div class="alert alert-success text-center" role="alert">
-                <strong>Well done!</strong> {{Session::get('message')}}
+                <strong>Berhasil Ditambahkan!</strong>
             </div>
         @endif
         <div class="widget-box">
             <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
-                <h5>List Products</h5>
+                <h5>Daftar Produk</h5>
             </div>
             <div class="widget-content nopadding">
                 <table class="table table-bordered data-table">
                     <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Image</th>
-                        <th>Product Name</th>
-                        <th>Under Category</th>
-                        <th>Code Of Product</th>
-                        <th>Product Color</th>
-                        <th>Price</th>
-                        <th>Image Gallery</th>
-                        <th>Add Attribute</th>
-                        <th>Action</th>
+                        <th>Gambar</th>
+                        <th>Nama Produk</th>
+                        <th>Kategori</th>
+                        <th>Kode Produk</th>
+                        <th>Varian Produk</th>
+                        <th>Harga</th>
+                        <th>Tambah Gambar</th>
+                        <th>Tambah atribut</th>
+                        <th>Aksi</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -38,13 +38,13 @@
                             <td style="vertical-align: middle;">{{$product->category->name}}</td>
                             <td style="vertical-align: middle;">{{$product->p_code}}</td>
                             <td style="vertical-align: middle;">{{$product->p_color}}</td>
-                            <td style="vertical-align: middle;">{{$product->price}}</td>
-                            <td style="vertical-align: middle;text-align: center;"><a href="{{route('image-gallery.show',$product->id)}}" class="btn btn-default btn-mini">Add Images</a></td>
-                            <td style="vertical-align: middle;text-align: center;"><a href="{{route('product_attr.show',$product->id)}}" class="btn btn-success btn-mini">Add Attr</a></td>
+                            <td style="vertical-align: middle;">Rp {{number_format($product->price, 0, ".", ".")}}</td>
+                            <td style="vertical-align: middle;text-align: center;"><a href="{{route('image-gallery.show',$product->id)}}" class="btn btn-default btn-mini">Tambah Gambar</a></td>
+                            <td style="vertical-align: middle;text-align: center;"><a href="{{route('product_attr.show',$product->id)}}" class="btn btn-success btn-mini">Tambah Atribut</a></td>
                             <td style="text-align: center; vertical-align: middle;">
-                                <a href="#myModal{{$product->id}}" data-toggle="modal" class="btn btn-info btn-mini">View</a>
+                                <a href="#myModal{{$product->id}}" data-toggle="modal" class="btn btn-info btn-mini">Tampil</a>
                                 <a href="{{route('product.edit',$product->id)}}" class="btn btn-primary btn-mini">Edit</a>
-                                <a href="javascript:" rel="{{$product->id}}" rel1="delete-product" class="btn btn-danger btn-mini deleteRecord">Delete</a>
+                                <a href="javascript:" rel="{{$product->id}}" rel1="delete-product" class="btn btn-danger btn-mini deleteRecord">Hapus</a>
                             </td>
                         </tr>
                         {{--Pop Up Model for View Product--}}
@@ -82,14 +82,14 @@
             var id=$(this).attr('rel');
             var deleteFunction=$(this).attr('rel1');
             swal({
-                title:'Are you sure?',
-                text:"You won't be able to revert this!",
-                type:'warning',
+             title:'Apakah anda yakin?',
+                text:"Data akan permanen diubah!",
+                type:'Peringatan!',
                 showCancelButton:true,
                 confirmButtonColor:'#3085d6',
                 cancelButtonColor:'#d33',
-                confirmButtonText:'Yes, delete it!',
-                cancelButtonText:'No, cancel!',
+                confirmButtonText:'Ya, hapus saja!',
+                cancelButtonText:'Tidak, batalkan!',
                 confirmButtonClass:'btn btn-success',
                 cancelButtonClass:'btn btn-danger',
                 buttonsStyling:false,
